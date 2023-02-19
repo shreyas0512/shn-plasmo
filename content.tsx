f652fb087f5e00e4680258a16f55a0b19ee47fcd
swethatheresa <swethatheresa123@gmail.com>
2023-02-19 07:24:26 +0530

    final_commit

--- content.tsx
+++ content.tsx
@@ -1,6 +1,6 @@
 import cssText from "data-text:~/style.css"
 import { useEffect, useState } from "react"
-
+import logo from "data-base64:~assets/logologo.png"
 import type { WikiMessage, WikiTldr } from "~background"
 
 export const getStyle = () => {
@@ -43,13 +43,38 @@ function IndexPopup() {
       text
     }: WikiMessage) {
       setDictionary(text)
+      generateText(text);
       return true
     })
-
+  
   },[]);
 
   const OpenAI = require('openai-api');
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
+const key=process.env.OPENAI_API_KEY;
+  const generateText = async (text) => {
+    const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${key}`,
+      },
+      body: JSON.stringify({
+        prompt: `What is ${text} ?`,
+        max_tokens: 50,
+        temperature: 0.5,
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+    setOpai(data.choices[0].text);
+  };
+
+ 
+
+
+
 
 
 
@@ -58,7 +83,7 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY);
     className="body1"
    >
     <div className="header">
-      
+      <img src={logo} style={{ width: "27px", height: "30px" }}></img>
       <h1>Simplify</h1>
       <h2>A tool to simplify your space</h2>
     </div>
@@ -70,7 +95,7 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY);
       <p>{definition}</p>
     </div>
     <div className="dict">
-      <h3>Queries</h3>
+      <h3>GPT-ANSWERS</h3>
       <p> {}</p>
       <h2 className="opai">{opai}</h2>
 
